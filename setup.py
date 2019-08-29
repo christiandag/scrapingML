7c5ff73289caec05678410722e8179d0cf551bc8
Christian Anasi <christiandag2@gmail.com>
2019-08-28 21:43:31 -0500

    update

--- setup.py
+++ setup.py
@@ -10,5 +10,5 @@ setup(
     long_description_content_type="text/markdown",
     url="https://microlab.ec",
     # packages=["C:/Users/Christian/Desktop/scraping_ml","scrapingML"]
-    packages=["https://github.com/christiandag/scrapingML/tree/master/scrapingML","scrapingML"]
+    # packages=["https://github.com/christiandag/scrapingML/tree/master/scrapingML","scrapingML"]
 )
