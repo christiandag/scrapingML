a46aa565c1be9e50817e8c55ee9925717e797e48
Christian Anasi <christiandag2@gmail.com>
2019-08-28 21:46:05 -0500

    update

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
-from setuptools import setup
+import setuptools
 
-setup(
+setuptools.setup(
     name="scrapingML",
     version="0.0.1",
     author="microlab.ec",
@@ -9,6 +9,7 @@ setup(
     # long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://microlab.ec",
+    packages=setuptools.find_packages()
     # packages=["C:/Users/Christian/Desktop/scraping_ml","scrapingML"]
     # packages=["https://github.com/christiandag/scrapingML/tree/master/scrapingML","scrapingML"]
 )
